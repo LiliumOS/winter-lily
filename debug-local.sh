cc2da7c4d1e11afbabbc5cce6f982ae8557300e8
Connor Horman <chorman64@gmail.com>
2025-03-03 23:27:52 -0500

    Support `target.sh` in debug-local.sh

--- debug-local.sh
+++ debug-local.sh
@@ -5,13 +5,7 @@ then
    GDB=gdb
 fi
 
-TARGET_PATH=debug
-
-
-if [ "$TARGET" \= "" ]
-then
-   TARGET="$(rustc --print host-tuple)"
-fi
+. ./target.sh
 
 if [ "$RELEASE" != "" ]
 then
@@ -22,5 +16,5 @@ TARGET="$TARGET" ./build.sh
 
 . ./local-env.sh
 
-exec $GDB $GDBARGS --args target/x86_64-unknown-linux-none/${TARGET_PATH}/libwl_ld_lilium.so "$@"
+exec $GDB $GDBARGS --args target/${TARGET_LD}/${TARGET_PATH}/libwl_ld_lilium.so "$@"
 
