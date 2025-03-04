8e5ae7dfffba1c1882229bd2cca1fedce944e3ce
Connor Horman <chorman64@gmail.com>
2025-03-03 21:15:51 -0500

    Add `/lib` to `local-env.sh`

--- local-env.sh
+++ local-env.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH_WL_HOST="$(pwd)/target/${TARGET}/${TARGET_PATH}:$(pwd)/target/${TARGET}/${TARGET_PATH}/deps:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH_WL_HOST="$(pwd)/target/${TARGET}/${TARGET_PATH}:$(pwd)/target/${TARGET}/${TARGET_PATH}/deps:${LD_LIBRARY_PATH}:${LD_LIBRARY_PATH_WL_HOST}:/lib"
 export WL_SUBSYS_base="target/${TARGET}/${TARGET_PATH}/libwl_usi_base.so"
