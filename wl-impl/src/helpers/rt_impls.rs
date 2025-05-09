698128c598f4975af846a8b54fc60ac03b56d3f0
Connor Horman <chorman64@gmail.com>
2025-05-09 09:01:41 -0400

    Apparently you can't call an FFI function, from a generic/inline function, in a dylib

--- wl-impl/src/helpers/rt_impls.rs
+++ wl-impl/src/helpers/rt_impls.rs
@@ -3,7 +3,7 @@ use core::arch::global_asm;
 use crate::helpers::CheckedAccessError;
 
 unsafe extern "C" {
-    pub unsafe fn __checked_memcpy_impl(
+    unsafe fn __checked_memcpy_impl(
         dest: *mut u8,
         src: *const u8,
         len: usize,
@@ -17,4 +17,14 @@ global_asm! {
     ".protected {__install_sa_handler}",
     __install_sa_handler = sym __install_sa_handler,
     __checked_memcpy_impl = sym __checked_memcpy_impl,
+}
+
+#[inline(never)]
+pub unsafe fn checked_memcpy_impl(
+    dest: *mut u8,
+    src: *const u8,
+    len: usize,
+    err: &mut CheckedAccessError,
+) -> isize {
+    unsafe { __checked_memcpy_impl(dest, src, len, err) }
 }
