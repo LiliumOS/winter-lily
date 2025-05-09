bd2358315be4739960bd36e2ffb4858843bb98b6
Connor Horman <chorman64@gmail.com>
2025-05-08 22:49:20 -0400

    Fix symbol name of `ExitThread`

--- wl-usi-thread/src/exit.rs
+++ wl-usi-thread/src/exit.rs
@@ -1,7 +1,7 @@
 use wl_impl::{export_syscall, libc::exit};
 
 export_syscall! {
-    unsafe extern fn ExitProcess(code: i32) -> ! {
+    unsafe extern fn ExitThread(code: i32) -> ! {
         unsafe { exit(code).unwrap() }
     }
 }
